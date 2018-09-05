0bd5254dbae239a5ec223fe8c5edd93c38753a17
zhangweitao <sk11zwt@126.com>
2018-09-05 21:04:09 +0800

    分页属性

--- src/main.js
+++ src/main.js
@@ -11,15 +11,15 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 // 引入格式化时间模块
 import moment from 'moment';
-// 设置 过滤器 格式化日期格式
-Vue.filter('fmtDate', (value, fmtStr) => {
-  return moment(value).format(fmtStr)
-});
 
 // 引入全局css文件
 import '@/assets/css/index';
 // 导入axios组件
 import http from '@/plugins/http';
+// 设置 过滤器 格式化日期格式
+Vue.filter('fmtDate', (value, fmtStr) => {
+  return moment(value).format(fmtStr);
+});
 // 注册 http组件
 Vue.use(http);
 // 注册element-ui插件
--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -1,79 +1,89 @@
 <template>
-  <div>
-<!--面包屑-->
-    <el-breadcrumb class="crumbs" separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-    </el-breadcrumb>
-<!-- / 面包屑-->
-<!-- 搜索框 -->
-  <el-row class="inp">
-    <el-col :span="24">
-      <el-input
-        style="width: 300px"
-        placeholder="请输入内容">
-        <el-button slot="append" icon="el-icon-search"></el-button>
-      </el-input>
-      <el-button type="success" plain>添加用户</el-button>
-    </el-col>
-  </el-row>    
-<!-- / 搜索框 -->
-<!-- 表格 -->
-    <template>
-  <el-table
-    v-loading="loading"
-    border
-    :data="tableData"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="username"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="email"
-      label="邮箱"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="mobile"
-      label="电话">
-    </el-table-column>
-    <el-table-column
-      prop="create_time"
-      label="时间">
-      <template slot-scope="scope" >
-        {{ scope.row.create_time | fmtDate('YYYY-MM-DD') }}
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="mg_state"
-      label="用户状态"
-      width="80">
-      <template slot-scope="scope">
-        <el-switch
-          v-model="scope.row.mg_state"
-          active-color="#13ce66"
-          inactive-color="#ff4949">
-        </el-switch>
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="操作">
-      <template slot-scope="scope">
-        <el-button plain size="mini" type="primary" icon="el-icon-edit"></el-button>
-        <el-button plain size="mini" type="success" icon="el-icon-check"></el-button>
-        <el-button plain size="mini" type="danger" icon="el-icon-delete"></el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-</template>
-<!-- / 表格 -->
-  </div>
+  <el-card class="box-card">
+    <!--面包屑-->
+      <el-breadcrumb class="crumbs" separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
+    <!--/面包屑-->
+    <!-- 搜索框-->
+      <el-row class="inp">
+        <el-col :span="24">
+          <el-input
+            style="width: 300px"
+            placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+          <el-button type="success" plain>添加用户</el-button>
+        </el-col>
+      </el-row>
+    <!--/搜索框-->
+    <!-- 表格-->
+        <el-table
+          v-loading="loading"
+          border
+          :data="tableData"
+          stripe
+          style="width: 100%">
+          <el-table-column
+            prop="username"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="email"
+            label="邮箱"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="mobile"
+            label="电话">
+          </el-table-column>
+          <el-table-column
+            prop="create_time"
+            label="时间">
+            <template slot-scope="scope" >
+              {{ scope.row.create_time | fmtDate('YYYY-MM-DD') }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="mg_state"
+            label="用户状态"
+            width="80">
+            <template slot-scope="scope">
+              <el-switch
+                v-model="scope.row.mg_state"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+              </el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="操作">
+            <template slot-scope="scope">
+              <el-button plain size="mini" type="primary" icon="el-icon-edit"></el-button>
+              <el-button plain size="mini" type="success" icon="el-icon-check"></el-button>
+              <el-button plain size="mini" type="danger" icon="el-icon-delete"></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+    <!--/表格-->
+    <!--分页-->
+      <el-pagination
+        class="paging"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pagenum"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+      </el-pagination>
+    <!--/分页-->
+  </el-card>
 </template>
 
 <script>
@@ -87,19 +97,30 @@ export default {
         mobile: '',
         mg_state: true
       }],
-       loading: true
+      loading: true,
+      pagenum: 4, // 页码
+      pagesize: 10, //页容量
+      total: 100 //总页数
     };
   },
   created () {
     this.loadData();
   },
   methods: {
+    // 分页相关方法
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`);
+    },
     // 异步请求列表数据
     async loadData () {
       // 设置token
       const token = sessionStorage.getItem('token');
       this.$http.defaults.headers.common['Authorization'] = token;
       const reponse = await this.$http.get(`users?pagenum=1&pagesize=3`);
+      this.loading = false;
       const {meta: {msg, status}} = reponse.data;
       if (status === 200) {
         this.tableData = reponse.data.data.users;
@@ -112,8 +133,11 @@ export default {
 </script>
 
 <style>
-  .len {
-    width: 30px;
+  .box-card {
+    height: 100%;
+  }
+  .paging {
+    margin: 20px 0;
   }
   .inp {
     margin: 15px 0;
