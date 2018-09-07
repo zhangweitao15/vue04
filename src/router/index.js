c33746dcc1ee5b8b78635c97ef856b87537dfeac
zhangweitao <sk11zwt@126.com>
2018-09-07 21:23:30 +0800

    打通权限列表组件

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,10 @@ import Router from 'vue-router';
 import Login from '@/views/login';
 import Home from '@/views/home';
 import Users from '@/views/users/List';
-// 导入 权限列表组件
+// 导入 角色列表组件
 import Rights from '@/views/rights/Rights';
-
+// 导入 权限列表组件  
+import Roles from '@/views/rights/Roles';
 Vue.use(Router);
 // 路由规则
 export default new Router({
@@ -25,6 +26,11 @@ export default new Router({
           name: 'rights',
           path: 'rights',
           component: Rights
+        },
+        {
+          name: 'roles',
+          path: 'roles',
+          component: Roles
         }
 
       ]}
---
+++ src/views/rights/Roles.vue
@@ -0,0 +1,17 @@
+<template>
+	<el-card class="box-roles">
+		15458
+	</el-card>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+	.box-roles {
+		height: 100%;
+	}
+</style>
