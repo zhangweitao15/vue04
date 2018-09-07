e6af52a8745247b30d1ecd1c8d59bea4f98813d6
zhangweitao <sk11zwt@126.com>
2018-09-07 14:59:37 +0800

    请求拦截器 设置token

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -4,6 +4,33 @@ const httpHelper = {};
 // 配置vue插件
 httpHelper.install = function (Vue) {
   axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/';
+  
+  // 添加请求拦截器 在拦截器中设置请求头token简化代码
+  axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    // 在请求发送之前，添加请求头token
+    // 添加请求头之前先判断 地址是不是/login
+    if (confin.url !== 'login') {
+      // 将token 从session.getitem 取出
+      const token = sessionStorage('token');
+      // 设置请求头
+      config.headers.Authorization = token;
+    }
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+  // 添加响应拦截器 处理加载提示
+  axios.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Do something with response error
+    return Promise.reject(error);
+  });
+  // 将axios 组件添加到vue对象中
   Vue.prototype.$http = axios;
 };
 export default httpHelper;
